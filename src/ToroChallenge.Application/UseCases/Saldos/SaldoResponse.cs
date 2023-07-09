8efca43800eb808ae8579c18b85d0666e1488447
Nelson Neto <nelson.ash@outlook.com>
2023-07-09 14:26:45 -0300

    Atualizar o SaldoResponse.cs

--- src/ToroChallenge.Application/UseCases/Saldos/SaldoResponse.cs
+++ src/ToroChallenge.Application/UseCases/Saldos/SaldoResponse.cs
@@ -7,7 +7,7 @@ namespace ToroChallenge.Application.UseCases.Saldos
         public decimal Valor { get; set; }
         public DateTime DataAtualizacao { get; set; }
 
-        public static SaldoResponse FromModel(Balance saldo)
+        public static explicit operator SaldoResponse FromModel(Balance saldo)
         {
             return new SaldoResponse() { Valor = saldo.Valor, DataAtualizacao = saldo.DataAtualizacao };
         }
